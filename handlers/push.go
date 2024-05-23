8a39e46f21354fc10064a9dfdfc7e795ddc33dd3
Moleus <fafufuburr@gmail.com>
2024-05-23 14:53:00 +0300

    start implementing call support

---
+++ handlers/push.go
@@ -0,0 +1,31 @@
+package handlers
+
+
+type PushEvent struct {
+    Event struct {
+        Payload Payload
+        Type string `json:"type"`
+    }
+}
+
+type Payload struct {
+    Actions       []Action `json:"actions"` // Assuming Action is a predefined struct in Go
+    EventTypeName string   `json:"event_type_name"`
+    ID            int      `json:"id"`
+    Message       string   `json:"message"`
+    PlaceID       int      `json:"place_id"`
+    Source        Source   `json:"source"` // Ensure there's a corresponding Go struct for Source
+    Timestamp     int64    `json:"timestamp"`
+}
+
+type Action string
+
+type Source struct {
+    ID   int    `json:"id"`
+    Type string `json:"type"`
+}
+
+// type Value struct {
+//     Type  string `json:"type,omitempty"`
+//     Value bool   `json:"value,omitempty"`
+// }
