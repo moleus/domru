53abac8be48771a53632117666a20457cdcf75bb
Andrew Kumpan <60757928+mrQmpan@users.noreply.github.com>
2025-11-05 02:06:59 +0200

    Update home.go

--- cmd/controllers/home.go
+++ cmd/controllers/home.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/moleus/domru/cmd/models"
-	"github.com/moleus/domru/pkg/authorizedhttp"
+	"github.com/mrqmpan/domru/cmd/models"
+	"github.com/mrqmpan/domru/pkg/authorizedhttp"
 )
 
 func (h *Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
