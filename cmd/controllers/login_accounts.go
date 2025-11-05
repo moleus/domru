d3d2d08475bb51bc39a3bd0ef2afb08412d0e859
Andrew Kumpan <60757928+mrQmpan@users.noreply.github.com>
2025-11-05 02:11:44 +0200

    Update login_accounts.go

--- cmd/controllers/login_accounts.go
+++ cmd/controllers/login_accounts.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/moleus/domru/cmd/models"
-	"github.com/moleus/domru/pkg/auth"
-	models2 "github.com/moleus/domru/pkg/domru/models"
+	"github.com/mrqmpan/domru/cmd/models"
+	"github.com/mrqmpan/domru/pkg/auth"
+	models2 "github.com/mrqmpan/domru/pkg/domru/models"
 )
 
 func (h *Handler) SelectAccountHandler(w http.ResponseWriter, r *http.Request) {
